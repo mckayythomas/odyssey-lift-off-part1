700784a6a67e068b6bdcd3cd32a8416ef230f66e
mckayythomas <mckayycollins@gmail.com>
2025-03-15 17:10:12 -0500

    graphql resolvers/ Server codegen types

---
+++ server/src/resolvers.ts
@@ -0,0 +1,14 @@
+import { Resolvers } from "./types";
+
+export const resolvers: Resolvers = {
+  Query: {
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+  },
+};
