3433c208729b4cca6cdbd42b6f3163535f927230
mckayythomas <mckayycollins@gmail.com>
2025-03-15 23:29:16 -0500

    finish adding client and server mutations

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -46,4 +46,19 @@ export const typeDefs = gql`
     "The module's length in minuets"
     length: Int
   }
+
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
 `;
